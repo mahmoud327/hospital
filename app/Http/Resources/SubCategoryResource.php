808e13c891b5cf4677c975eeeeb4cdd647f5bb0d
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-10-29 16:43:35 +0200

    F

--- app/Http/Resources/SubCategoryResource.php
+++ app/Http/Resources/SubCategoryResource.php
@@ -11,7 +11,7 @@ class SubCategoryResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'title' => $this->title,
+            'name' => $this->name,
             'image' => $this->image_path,
 
             'parent_id' => $this->parent_id,
