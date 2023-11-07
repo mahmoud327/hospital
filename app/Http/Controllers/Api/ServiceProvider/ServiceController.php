e0c8133cc96d16c1eb075bdd701e2cf20472aec0
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-11-07 18:30:18 +0200

    colors

--- app/Http/Controllers/Api/ServiceProvider/ServiceController.php
+++ app/Http/Controllers/Api/ServiceProvider/ServiceController.php
@@ -18,29 +18,29 @@ class ServiceController extends Controller
 
     public function index(Request $request)
     {
-
-        $services=ServiceProviderService::query()
-            ->where('service_provider_id',auth()->user()->service_provider_id)
+        $services = ServiceProviderService::query()
+            ->where('service_provider_id', auth()->user()->service_provider_id)
             ->with('service')
             ->latest()
             ->paginate(10);
-            return JsonResponse::json('ok', ['data' => ServiceProviderServiceResource::collection($services)]);
-
-
+        return JsonResponse::json('ok', ['data' => ServiceProviderServiceResource::collection($services)]);
     }
     public function store(Request $request)
     {
-
-        $this->validate($request, [
-            'services' => 'required|array',
-            'price' => 'required|numeric'
-        ]);
-        foreach ($request->services as $serviceId) {
-            ServiceProviderService::updateorcreate([
-                'service_id' => $serviceId,
-                'price' => $request->price,
-                'service_provider_id' => auth()->user()->service_provider_id
-            ]);
+        // $this->validate($request, [
+        //     'services' => 'required|array',
+        //     'price' => 'required|numeric'
+        // ]);
+        foreach ($request->services as $service) {
+            $serviceId = $service['id'];
+            $price = $service['price'];
+            ServiceProviderService::updateOrCreate(
+                [
+                    'service_id' => $serviceId,
+                    'service_provider_id' => auth()->user()->service_provider_id,
+                ],
+                ['price' => $price]
+            );
         }
         return sendJsonResponse([], 'service provider added service sucessfully');
     }
